3f6adf1cc563b7719316d09c0b6bc44262e5f8fe
Ivo Hradek <ivohradek@gmail.com>
2017-02-26 00:52:17 +0100

    Added main characters

--- database/seeds/CharactersTableSeeder.php
+++ database/seeds/CharactersTableSeeder.php
@@ -13,12 +13,16 @@ class CharactersTableSeeder extends Seeder
     public function run()
     {
         DB::table('characters')->insert([
-            ['id' => 1, 'nick' => 'leela', 'name' => 'Leela Turange'],
-            ['id' => 2, 'nick' => 'fry', 'Fry'],
-            ['id' => 3, 'nick' => 'bender', 'Bender'],
-            ['id' => 4, 'nick' => 'professor', 'Professor Hubert Farnsworth'],
-            ['id' => 5, 'nick' => 'amy', 'Amy Lee Wong'],
-            ['id' => 6, 'nick' => 'hermes', 'Hermes Condrad'],
+            ['id' => 1, 'nick' => 'leela', 'name' => 'Turanga Leela'],
+            ['id' => 2, 'nick' => 'fry', 'name' =>'Philip J. Fry'],
+            ['id' => 3, 'nick' => 'bender', 'name' => 'Bender Bending Rodriguez'],
+            ['id' => 4, 'nick' => 'professor', 'name' => 'Professor Hubert J. Farnsworth'],
+            ['id' => 5, 'nick' => 'amy', 'name' => 'Amy Wong'],
+            ['id' => 6, 'nick' => 'hermes', 'name' => 'Hermes Condrad'],
+            ['id' => 7, 'nick' => 'zoidberg', 'name' => 'Doctor John Zoidberg'],
+            ['id' => 8, 'nick' => 'nibbler', 'name' => 'Lord Nibbler'],
+            ['id' => 9, 'nick' => 'zapp', 'name' => 'Zapp Brannigan'],
+            ['id' => 10, 'nick' => 'kif', 'name' => 'Kif Kroker'],
         ]);
     }
 }
