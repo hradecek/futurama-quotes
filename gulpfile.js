c04f1a67aa52e07e841c8b1d988a8a108b485e5f
hradecek <ivohradek@gmail.com>
2017-04-03 00:34:21 +0200

    Init gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* Imports */
+var gulp = require('gulp');
+var sassc = require('gulp-sass');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-clean-css');
+
+/* Directories */
+var cssDir = 'public/css/';
+var sassDir = 'resources/assets/sass/';
+
+gulp.task('sass', function () {
+    gulp.src(sassDir + '/**/*.scss')
+        .pipe(sassc())
+        .pipe(minifyCss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(cssDir))
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch(sassDir + '/**/*.scss')
+});
--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -1,9 +1,23 @@
 <html>
 <head>
-  <title>Futurama Quotes</title>
+  <title>Futurama {{ trans('quotes') }}</title>
+  <link rel="stylesheet" href="css/styles.min.css"/>
 </head>
-<body style="background-color: #0F446F">
-  <h1>Quote:</h1>
-  <p>{{ $quote }}</p>
+<body>
+  <!-- Title -->
+  <div>
+    <h1 class="title">Futurama {{ trans('quotes') }}</h1>
+  </div>
+  <!-- Content -->
+  <div>
+    <p class="quote">{{ $quote }}</p>
+  </div>
+  <!-- Footer -->
+  <div>
+    <p>
+      <img src="img/planet_express_logo.png" alt="Planet Express Logo" height="50"/>
+      Provided by Planet Express, Inc
+    </p>
+  </div>
 </body>
 </html>
