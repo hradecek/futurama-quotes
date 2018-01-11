d0c6d178a9c7962876bd221d1852bb6f981f6aab
hradecek <ivohradek@gmail.com>
2018-01-11 15:07:38 +0100

    Rework

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -5,26 +5,24 @@
 </head>
 <body>
   <!-- Title -->
-  <div>
+  <div class="header">
     <h1 class="title" title="FUTURAMA {{ strtoupper(trans('quotes')) }}">
       <div>FUTURAMA {{ strtoupper(trans('quotes')) }}</div>
     </h1>
   </div>
   <!-- Content -->
-  <div>
+  <div class="content">
     <blockquote class="oval-quote">
       <p>{{ $quote }}</p>
     </blockquote>
-  </div>
-  <div class="character-container">
-    <img class="character" src="{{ $characterImgUri }}" alt=""/>
+    <div class="character-container">
+      <img class="character" src="{{ $characterImgUri }}" alt=""/>
+    </div>
   </div>
   <!-- Footer -->
   <div class="footer">
-    <div class="footer-right">
       <img class="planet-express-logo" src="img/planet_express_logo50.png" alt="Planet Express Logo" height="50">
       Provided by Planet Express, Inc.
-    </div>
   </div>
 </body>
 </html>
