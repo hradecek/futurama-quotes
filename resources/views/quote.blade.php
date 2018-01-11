fff9455e6742c7aaa0ed6293303d058b550c1fbb
hradecek <ivohradek@gmail.com>
2018-01-11 01:03:23 +0100

    Few catchs

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -6,22 +6,24 @@
 <body>
   <!-- Title -->
   <div>
-    <h1 class="title">Futurama {{ trans('quotes') }}</h1>
+    <h1 class="title" title="FUTURAMA {{ strtoupper(trans('quotes')) }}">
+      <div>FUTURAMA {{ strtoupper(trans('quotes')) }}</div>
+    </h1>
   </div>
   <!-- Content -->
   <div>
     <blockquote class="oval-quote">
       <p>{{ $quote }}</p>
     </blockquote>
-    <img class="character" src="{{ $characterImgUri }}" alt="" />
+  </div>
+  <div class="character-container">
+    <img class="character" src="{{ $characterImgUri }}" alt=""/>
   </div>
   <!-- Footer -->
   <div class="footer">
-    <div class="footer-left">
-      <p>
-        <img src="img/planet_express_logo.png" alt="Planet Express Logo" height="50"/>
-        Provided by Planet Express, Inc
-      </p>
+    <div class="footer-right">
+      <img class="planet-express-logo" src="img/planet_express_logo50.png" alt="Planet Express Logo" height="50">
+      Provided by Planet Express, Inc.
     </div>
   </div>
 </body>
