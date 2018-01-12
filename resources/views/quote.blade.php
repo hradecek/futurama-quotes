d0e4d9bde4f2943ec3011406121048d171dfae96
hradecek <ivohradek@gmail.com>
2018-01-12 01:29:05 +0100

    Added side-bar

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -10,6 +10,17 @@
       <div>FUTURAMA {{ strtoupper(trans('quotes')) }}</div>
     </h1>
   </div>
+  <!-- Character's Menu -->
+  <div class="heads-menu">
+    <button class="head-button" type="submit">
+      <img class="head-icon" src="img/characters/icons/fry_head.ico" alt="Fry's quotes"><br>
+      <img class="head-icon" src="img/characters/icons/bender_head.ico" alt="Bender's quotes"><br>
+      <img class="head-icon" src="img/characters/icons/leela_head.ico" alt="Leela's quotes"><br>
+      <img class="head-icon" src="img/characters/icons/professor_head.png" alt="Professor's quotes"><br>
+      <img class="head-icon" src="img/characters/icons/zoidberg_head.png" alt="Dr. Zoidberg's quotes"><br>
+      <img class="head-icon" src="img/characters/icons/amy_head.png" alt="Amy's quotes">
+    </button>
+  </div>
   <!-- Content -->
   <div class="content">
     <blockquote class="oval-quote">
@@ -23,7 +34,7 @@
   <div class="footer">
     <div class="city-bg"></div>
     <img class="planet-express-logo" src="img/planet_express_logo50.png" alt="Planet Express Logo" height="50">
-    Provided by Planet Express, Inc.
+    <span class="provided">Provided by Planet Express, Inc.</span>
   </div>
 </body>
 </html>
